27b20c9d031906c5c42d2ad2fb47cb26b928f731
Tom Atnip <atnip@sep.com>
2015-06-02 20:22:54 -0400

    Refactoring query class

--- Sportz/src/QueryGenerator.py
+++ Sportz/src/QueryGenerator.py
@@ -2,37 +2,49 @@ import requests
 
 BASE_URL = "http://api.sportsdatabase.com/nfl/query.json"
 
-def GetTeamConferenceDivision(season):
-  query = "team,conference,division@season={}".format(season)
-  payload = {"output" : "json", "api_key" : "guest", "sdql" : query}
-  result = requests.get(BASE_URL, params=payload)
-  jsonResult = _FormatStringToJSON(result.text)
-
-  teams = {}
-  data = jsonResult['groups'][0]['columns']
-
-  for i in range(len(data[0])):
-    teams[data[0][i]] = {"conference": data[1][i], "division": data[2][i]}
-
-  return teams
-
-def GetTeamScheduleDatesOpponents(team, season):
-  query = "date,o:team@team={0} and season={1}".format(team, season)
-  payload = {"output" : "json", "api_key" : "guest", "sdql" : query}
-  result = requests.get(BASE_URL, params=payload)
-  jsonResult = _FormatStringToJSON(result.text)
-
-  games = {}
-  data = jsonResult['groups'][0]['columns']
+class KillerSportsTeam:
+  def __init__(self, team, season):
+    self.team = team
+    self.season = season
   
-  for i in range(len(data[0])):
-    games[data[0][i]] = {"opponent": data[1][i]}
+  def get_team_biggest_lead(self, week):
+    return self._get_game_parameter("biggest lead", week)
     
-  return games
-
-def _FormatStringToJSON(result):
-  result = result.replace('json_callback(', '')
-  result = result.replace(');\n', '')
-  result = result.replace('\t', '')
-  return eval(result)
+  def get_team_blocked_extra_points(self, week):
+    return self._get_game_parameter("blocked extra points", week)
   
+  def get_team_blocked_punts(self, week):
+    return self._get_game_parameter("blocked punts", week)
+  
+  def get_team_conference(self):
+    query = "conference@team={0} and season={1}".format(self.team, self.season)
+    payload = {"output" : "json", "api_key" : "guest", "sdql" : query}
+    result = requests.get(BASE_URL, params=payload)
+    jsonResult = self._format_string_to_JSON(result.text)
+    
+    return jsonResult['groups'][0]['columns'][0][0]
+  
+  def get_team_division(self):
+    query = "division@team={0} and season={1}".format(self.team, self.season)
+    payload = {"output" : "json", "api_key" : "guest", "sdql" : query}
+    result = requests.get(BASE_URL, params=payload)
+    jsonResult = self._format_string_to_JSON(result.text)
+    
+    return jsonResult['groups'][0]['columns'][0][0]
+  
+  def get_team_opponent(self, week):
+    return self._get_game_parameter("o:team", week)
+  
+  def _get_game_parameter(self, parameter, week):
+    query = "{0}@team={1} and season={2} and week={3}".format(parameter, self.team, self.season, week)
+    payload = {"output" : "json", "api_key" : "guest", "sdql" : query}
+    result = requests.get(BASE_URL, params=payload)
+    jsonResult = self._format_string_to_JSON(result.text)
+    
+    return jsonResult['groups'][0]['columns'][0][0]
+  
+  def _format_string_to_JSON(self, result):
+    result = result.replace('json_callback(', '')
+    result = result.replace(');\n', '')
+    result = result.replace('\t', '')
+    return eval(result)
--- Sportz/src/QueryGeneratorTest.py
+++ Sportz/src/QueryGeneratorTest.py
@@ -1,31 +1,67 @@
-import MockResults
 import QueryGenerator
 from unittest import TestCase, mock, main
 
-class InsertionSortTests(TestCase):
+class QueryGeneratorTest(TestCase):
   def setUp(self):
+    self.test_team = QueryGenerator.KillerSportsTeam("Colts", 2014)
     self.page_request = mock.MagicMock()
   
   def tearDown(self):
     pass
 
   @mock.patch('QueryGenerator.requests.get')
-  def test_GetTeamsConferenceAndDivision(self, mock_request):
-    self.page_request.text = MockResults.TEAMS_CONFERENCE_AND_DIVISION
+  def test_GetTeamConference(self, mock_request):
+    self.page_request.text = 'json_callback({  "headers": [\'conference\'], "groups": [ {    "sdql": "team = Colts and season = 2014 and week = 1" ,   "columns" : [     [\'AFC\']   ]}   ] });\n'
     mock_request.return_value = self.page_request
     
-    actual = QueryGenerator.GetTeamConferenceDivision(2014)    
-    mock_request.assert_called_once_with("http://api.sportsdatabase.com/nfl/query.json", params={"output" : "json", "api_key" : "guest", "sdql" : "team,conference,division@season=2014"})
-    self.assertEqual(MockResults.TEAMS_CONFERENCE_AND_DIVISION_SOLUTION, actual)
+    actual = self.test_team.get_team_conference()    
+    mock_request.assert_called_once_with("http://api.sportsdatabase.com/nfl/query.json", params={"output" : "json", "api_key" : "guest", "sdql" : "conference@team=Colts and season=2014"})
+    self.assertEqual("AFC", actual)
   
   @mock.patch('QueryGenerator.requests.get')
-  def test_GetTeamScheduleDatesAndOpponents(self, mock_request):
-    self.page_request.text = MockResults.TEAMS_SCHEDULE_AND_OPPONENT
+  def test_GetTeamDivision(self, mock_request):
+    self.page_request.text = 'json_callback({  "headers": [\'division\'], "groups": [ {    "sdql": "team = Colts and season = 2014" ,   "columns" : [     [\'AFC South\',\'AFC South\',\'AFC South\',\'AFC South\',\'AFC South\',\'AFC South\',\'AFC South\',\'AFC South\',\'AFC South\',\'AFC South\',\'AFC South\',\'AFC South\',\'AFC South\',\'AFC South\',\'AFC South\',\'AFC South\',\'AFC South\',\'AFC South\',\'AFC South\']   ]}   ] });\n'
     mock_request.return_value = self.page_request
     
-    actaul = QueryGenerator.GetTeamScheduleDatesOpponents("Colts", 2014)
-    mock_request.assert_called_once_with("http://api.sportsdatabase.com/nfl/query.json", params={"output" : "json", "api_key" : "guest", "sdql" : "date,o:team@team=Colts and season=2014"})
-    self.assertEqual(MockResults.TEAMS_SCHEDULE_AND_OPPONENT_SOLUTION, actaul)
+    actual = self.test_team.get_team_division()
+    mock_request.assert_called_once_with("http://api.sportsdatabase.com/nfl/query.json", params={"output" : "json", "api_key" : "guest", "sdql" : "division@team=Colts and season=2014"})
+    self.assertEqual("AFC South", actual)
+  
+  @mock.patch('QueryGenerator.requests.get')
+  def test_GetTeamOpponent(self, mock_request):
+    self.page_request.text = 'json_callback({  "headers": [\'o:team\'], "groups": [ {    "sdql": "team = Colts and season = 2014 and week = 1" ,   "columns" : [     [\'Broncos\']   ]}   ] });\n'
+    mock_request.return_value = self.page_request
+    
+    actual = self.test_team.get_team_opponent(1)
+    mock_request.assert_called_once_with("http://api.sportsdatabase.com/nfl/query.json", params={"output" : "json", "api_key" : "guest", "sdql" : "o:team@team=Colts and season=2014 and week=1"})
+    self.assertEqual("Broncos", actual)
+    
+  @mock.patch('QueryGenerator.requests.get')
+  def test_GetTeamBiggestLead(self, mock_request):
+    self.page_request.text = 'json_callback({  "headers": [\' biggest lead\'], "groups": [ {    "sdql": "team = Colts and season = 2014 and week = 1" ,   "columns" : [     [0]   ]}   ] });\n'
+    mock_request.return_value = self.page_request
 
+    actual = self.test_team.get_team_biggest_lead(1)
+    mock_request.assert_called_once_with("http://api.sportsdatabase.com/nfl/query.json", params={"output" : "json", "api_key" : "guest", "sdql" : "biggest lead@team=Colts and season=2014 and week=1"})
+    self.assertEqual(0, actual)
+
+  @mock.patch('QueryGenerator.requests.get')
+  def test_GetTeamBlockedExtraPoints(self, mock_request):
+    self.page_request.text = 'json_callback({  "headers": [\'blocked extra points\'], "groups": [ {    "sdql": "team = Colts and season = 2014 and week = 1" ,   "columns" : [     [0]   ]}   ] });\n'
+    mock_request.return_value = self.page_request
+
+    actual = self.test_team.get_team_blocked_extra_points(1)
+    mock_request.assert_called_once_with("http://api.sportsdatabase.com/nfl/query.json", params={"output" : "json", "api_key" : "guest", "sdql" : "blocked extra points@team=Colts and season=2014 and week=1"})
+    self.assertEqual(0, actual)
+
+  @mock.patch('QueryGenerator.requests.get')
+  def test_GetTeamBlockedPunts(self, mock_request):
+    self.page_request.text = 'json_callback({  "headers": [\'blocked punts\'], "groups": [ {    "sdql": "team = Colts and season = 2014 and week = 1" ,   "columns" : [     [0]   ]}   ] });\n'
+    mock_request.return_value = self.page_request
+
+    actual = self.test_team.get_team_blocked_punts(1)
+    mock_request.assert_called_once_with("http://api.sportsdatabase.com/nfl/query.json", params={"output" : "json", "api_key" : "guest", "sdql" : "blocked punts@team=Colts and season=2014 and week=1"})
+    self.assertEqual(0, actual)
+    
 if __name__ == '__main__':
   main()
